d911bc5fd8baa2fe71a5e325e25eb6d2019e44b3
Vedant Narayankeri <nsvedant2000@gmail.com>
2026-05-02 01:47:43 +0530

    LinkedLIst solution

--- Arrays&Hashing/705-design-hashset/Solution.java
+++ Arrays&Hashing/705-design-hashset/Solution.java
@@ -1,3 +1,4 @@
+/*
 class MyHashSet {
 
     private List<Integer> array;
@@ -20,6 +21,72 @@ class MyHashSet {
     
     public boolean contains(int key) {
         return array.contains(key);
+    }
+}*/
+
+class MyHashSet {
+    //Linked-list Implementation
+    class Node{
+        int val;
+        Node next;
+        Node(int key){
+            this.val = key;
+        }
+    }
+    //Index = hash(data)
+    //At index we put data and continue extending nodes when same hash found
+
+    //Max number of operations = 10^4 that means, at max we can potentially add 10^4 elements
+    //Setting this as size, doesn't matter what are numbers inserted as we compute hash
+    private static int MAX_SIZE = 10000;
+    private Node[] buckets;
+
+    public MyHashSet() {
+        buckets = new Node[MAX_SIZE];
+        for(int i = 0;i<MAX_SIZE;i++){
+            buckets[i] = new Node(0);// Default node at each bucket start
+        }
+    }
+    
+    public void add(int key) {
+        Node node = buckets[getHashIndex(key)];
+        //For iteration to reach last node
+        while(node.next != null){
+            if(node.next.val == key){
+                return;//Exit if key already exists
+            }
+            node = node.next;
+        }
+        //Base case
+        node.next = new Node(key);
+    }
+    
+    public void remove(int key) {
+        Node node = buckets[getHashIndex(key)];
+        //Simple removal from list
+        while(node.next != null){
+            if(node.next.val == key){
+                //prev - node
+                node.next = node.next.next;
+                return;//Exit once done
+            }
+            node = node.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        Node node = buckets[getHashIndex(key)];
+        while(node.next != null){
+            if(node.next.val == key){
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    private int getHashIndex(int key){
+        return key%MAX_SIZE;
     }
 }
 
